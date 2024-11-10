1224bcc090a5e2fb53692285974b5a6be856c5ad
Maksim Shubny <shubny.maksim@yandex.ru>
2024-11-11 01:55:25 +0500

    refactor router

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import HeroView from '../views/HeroView';
-import OurCoffee from '../views/OurCoffee';
-import YourPlesure from '../views/YourPlesure';
-import ContactsView from '../views/ContactsView';
-import ThanksView from '../views/ThanksView';
+// import HeroView from '../views/HeroView';
+// import OurCoffee from '../views/OurCoffee';
+// import YourPlesure from '../views/YourPlesure';
+// import ContactsView from '../views/ContactsView';
+// import ThanksView from '../views/ThanksView';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: HeroView },
-  { path: '/our-coffee', component: OurCoffee },
-  { path: '/your-plesure', component: YourPlesure },
-  { path: '/contacts', component: ContactsView },
-  { path: '/thanks', component: ThanksView },
+  { path: '/', component: () => import('../views/HeroView') },
+  { path: '/our-coffee', component: () => import('../views/OurCoffee') },
+  { path: '/your-plesure', component: () => import('../views/YourPlesure') },
+  { path: '/contacts', component: () => import('../views/ContactsView') },
+  { path: '/thanks', component: () => import('../views/ThanksView') },
 ];
 
 const router = new VueRouter({
